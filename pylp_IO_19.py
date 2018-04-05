00802cfecd354144f90318451e3ead32cb127774
alignwaives <alignwaives@gmail.com>
2018-04-05 16:54:01 -0700

    refactoring sl script into main

--- pylp_IO_19.py
+++ pylp_IO_19.py
@@ -4,29 +4,11 @@ from OSC import OSCServer,OSCClient, OSCMessage
 from time import sleep
 import types, threading
 
+import sl_indy_4
+
 
 '''python script to control sooperlooper via open sound control (OSC), midi output for instruments and the launchpad '''
 
-
-class Sl():                                 
-    def __init__(self):
-        self.states = {}
-
-        self.cmds = [ "record", "overdub", "replace", "insert",
-                  "pause","oneshot","undo", "redo"]
-
-    '''      #/sl/#/set s:control f:value
-                ["sync"
-              "relative_sync"]
-                #set_sync_pos '''
-    
-    def command(self, x, y):
-        if x < 8:
-            print self.cmds[x]
-            try:
-                slcli.send( OSCMessage("/sl/" + str(y) + "/down", Soop.cmds[x]) )
-            except:
-                print "Connection refused"
 
 
 class Lpad_lights():
@@ -37,14 +19,14 @@ class Lpad_lights():
         self.mode = 0
 
 
-        self.fg = {} #background
+        self.fg = {} #foreground
         fg = self.fg
         for x in range(9):
             for y in range(9):
                 fg[ (x , y,) ] =  [0,3]
         del fg[(8,8)]
         
-        self.bg = self.fg.copy() #foreground
+        self.bg = self.fg.copy() #background
         self.bg1 = fg.copy()
         self.bg2 = fg.copy()
         
@@ -56,18 +38,15 @@ class Lpad_lights():
         
         self.layer_sel = [fg, bg, bg1, bg2]  # for multiple modes
 
-        self.pressed = {}
+        self.pressed = {} #for tracking currently pressed buttons
         pressed = self.pressed
         
         
-        for items in bg1:
+        for items in bg1: #random background
             self.bg1[items] = [random.randint(0,3), random.randint(0,3)]
 
 
-        for r in range(4):
-            print r
-            #r = -1 * r + 3
-            print '-', r
+        for r in range(4): #colorful, ordered background
             for c in range(4):
                 bg[(r,c)] = [-r + 3, -c + 3]
                 bg[(r + 4,c)] = [r, -c + 3]
@@ -83,31 +62,29 @@ class Lpad_lights():
         for i in layer:
             layer[i] = [red, green]
         
-
-    def mode_switch(self, mode):
+ 
+    def bg_switch(self, mode):
         '''switch mode''' 
-        
-        Pad.mode = mode
         newbg = self.layer_sel[mode]
-        print 'mode_switch', mode
+        print 'bg_switch', mode
         for buttons in newbg:
             l.light(buttons[0], buttons[1], newbg[buttons][0], newbg[buttons][1])
         
 
-    def lighty(self, x, y, vel):
+    def lighty(self, x, y, vel, inv=1):
         '''light up individual button presses'''
         #depending on Pad.lmode, presses will light different colors and do different things
-        layer = Pad.layer_sel[Pad.mode]
+        if Pad.mode == 0 and inv == 1:
+               #layer_sel[Pad.mode]
+            bg = Pad.bg
 
-        red = -layer[(x, y)][0] + 3 # invert foreground color from background colors
-        green = -layer[(x, y)][1] + 3
+            red = -bg[(x, y)][0] + 3 # invert foreground color from background colors
+            green = - bg[(x, y)][1] + 3      
 
-        bg = Pad.layer_sel[Pad.mode]
-
-        if vel: #button press
-            l.light(x, y, red, green)
-        elif vel == 0:
-            l.light(x, y, bg[(x, y,)][0], bg[(x, y,)][1])
+            if vel: #button press
+                l.light(x, y, red, green)
+            elif vel == 0:
+                l.light(x, y, bg[(x, y,)][0], bg[(x, y,)][1])
 
 def lp_input():
     '''get input from launchpad'''
@@ -119,63 +96,33 @@ def lp_input():
                 event[2] =int(event[2])
                 event.append(event[2])
                 x,y = event[0],event[1]
-                
-                note = 0
-
             
-                
-                #this should probably be a list comprehension
-                if x == 8:
-                    note = y + 72
+                note = 8 * y + x
 
-                elif y == 0:
-                    note = x
-                elif y == 1:
-                    note = x + 8
-                elif y == 2:
-                    note = x + 16
-                elif y == 3:
-                    note = x + 24
-                elif y == 4:
-                    note = x + 32
-                elif y == 5:
-                    note = x + 40
-                elif y == 6:
-                    note = x + 48
-                elif y == 7:
-                    note = x + 56 #top row of grid
-                elif y == 8:
-                    note = x + 64 #top row (controls)
-
-                note = 0
-                
-                print note
-
-                
+                midis = [midi_low, midi_mid, midi_high, midi_masha]
 
                 if note < Pad.lowbound:   #seperate to different midi ports
-                    midisel = midiout
+                    midisel = midis[0]
                 elif note < Pad.highbound:
-                    midisel = midiout2
+                    midisel = midis[1]
                 else:
-                    midisel = midiout3
+                    midisel = midis[2]
 
-                if event[2] == 1:
+                if event[2] == 1: #note on
                     #todo - if SL enabled in mode
-                    Soop.command(x, y)                    
+                    '''Soop.command(x, y) refactor '''                    
                     midisel.send_message([0x90, note, event[2]*127 ])
-                    Pad.lighty(event[0], event[1], 1)
-
+                    
+                    Pad.lighty(x, y, 1)
+                        
                     if (x,y) not in Pad.pressed:
                         Pad.pressed[x, y] = 1
 
                     #print Pad.pressed
                         
-                elif event[2] == 0:
+                elif event[2] == 0: #note off
                     midisel.send_message([0x80, note, event[2]])                    
-                    Pad.lighty(event[0], event[1], 0)
-
-                    
+                    Pad.lighty(x, y, 0)
                     
                     if (x,y) in Pad.pressed:
                         del Pad.pressed[x, y]
@@ -187,16 +134,7 @@ def handle_error(self,request,client_address):
     print self, request, client_address ##additional details
 
 
-def lp_mode(path, tags, args, source):
-    if path == '/mode':
-
-        #stopthread argument
-        if args[0] == "stopthread":
-            Pad.thread = 0
-        
-        print 'lpmode', args
-        Pad.mode_switch(args)
-        
+  
 
 def lp_callback(path, tags, args, source):
     x, y = args[:2]
@@ -221,52 +159,45 @@ def server_thread():
     while Pad.thread == 1:
         server.handle_request()
 
-def callback(path, tags, args, source):
-    print 'callllllllback',  args
-
 def input_thread():
     while Pad.thread == 1:
         lp_input()
-        
-    
-def pedal():
-    pass
 
+def sl_osc():
+  pass  
+        
 if __name__=="__main__":
 
     server = OSCServer(("127.0.0.1", 8000))
     server.addMsgHandler("/lp", lp_callback) #for button presses (foreground)
     server.addMsgHandler("/lp2", lp_background) #for background
-    server.addMsgHandler("/mode", lp_mode)
-    server.addMsgHandler( "/reg", callback)
-    server.addMsgHandler( "/ping", callback)
-    server.addMsgHandler("/ped", pedal)
+    #server.addMsgHandler( "/ping", callback)
+    
     
     server.handle_error = types.MethodType(handle_error, server)
 
 
-    #OSC
-    client = OSCClient() #send to python script
-    client.connect( ("localhost", 8000) )
-
     slcli = OSCClient() #send to sooperlooper
     slcli.connect( ("localhost", 9951) ) 
 
-    
+    Soop = sl_indy_4.Loop()
+    Soop.command(2, 3)
+    command(2,3)
+
     #initialize midi outputs
-    midiout = rtmidi.MidiOut(name = "RtMidi Low: drums")
-    midiout2 = rtmidi.MidiOut(name = "RtMidi Middle: inst")
-    midiout3 = rtmidi.MidiOut(name = "RtMidi High: controls")
+    midi_low = rtmidi.MidiOut(name = "RtMidi Low:")
+    midi_mid = rtmidi.MidiOut(name = "RtMidi Middle:")
+    midi_high = rtmidi.MidiOut(name = "RtMidi High:")
     midi_masha = rtmidi.MidiOut(name = "RtMidi Masha")
-    available_ports = midiout.get_ports()
+
+    
+    available_ports = midi_low.get_ports()
     
     if available_ports:
-        midiout.open_port(0)
-        midiout2.open_port(0)
-        midiout3.open_port(0)
+        midi_low.open_port(0)
+        midi_mid.open_port(0)
+        midi_high.open_port(0)
         midi_masha.open_port(0)
-    else:
-        midiout.open_virtual_port("My virtual output")
 
 
     #init launchpad
@@ -282,27 +213,37 @@ if __name__=="__main__":
     except IndexError:
         print "launchpad not found"
 
-
-    Soop = Sl()
-
-    Pad = Lpad_lights()
+    Pad = Lpad_lights() #init
     Pad.monochrome(0, 0, 3) #fg
-    Pad.monochrome(3, 1, 2)
-
-    sleep(.5)
+    Pad.monochrome(3, 1, 2) #bg2
+    
 
     #display / cycle through modes, end with bg (default background)
     for i in range(3):
-        Pad.mode_switch(3)
-        Pad.mode_switch(2)
-        Pad.mode_switch(0)   
-        Pad.mode_switch(1)
+        Pad.bg_switch(3)
+        Pad.bg_switch(2)
+        Pad.bg_switch(0)   
+        Pad.bg_switch(1)
         sleep(.3)
     #begin threads, should loop based on functions
     t = threading.Thread(name='pad inputs', target=input_thread) #switched to threading OSC as latency affectd OSC handler
     t.start()
 
-    sleep(1)
+    
+    Soop = sl_indy_4.Loop(slcli)
+    Soop.command(2, 3)
+    command(2,3)
+
+    for i in range(2): #must do twice to determine the connection failed?
+        try:
+            slcli.send( OSCMessage("/ping", ["localhost:8000", '/sl'] ) )
+        except:
+            print "Connection refused"
+            #Pad.mode == 'off'
+
+    #register for loops being added        
+    slicli.send( OSCMessage("/register", ["localhost:8000", '/sl'] ) )
+    
 
     while 1:
         try:
